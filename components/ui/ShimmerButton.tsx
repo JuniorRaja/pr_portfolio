1595786eaa1ce5a4725b106192d297374dfbf25c
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-07-12 09:12:15 +0530

    feat: Update ShimmerButton component with customCSS prop

--- components/ui/ShimmerButton.tsx
+++ components/ui/ShimmerButton.tsx
@@ -1,7 +1,13 @@
 "use client";
 import { useState } from "react";
 
-const ShimmerButton = ({ title }: { title: string }) => {
+const ShimmerButton = ({
+  title,
+  customCSS,
+}: {
+  title: string;
+  customCSS?: string;
+}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const handleMouseMovement = () => {
@@ -11,12 +17,12 @@ const ShimmerButton = ({ title }: { title: string }) => {
   return (
     <button
       className={`inline-flex h-12 animate-shimmer items-center justify-center rounded-md border backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200
-            border-slate-800 bg-[length:200%_100%] px-6 
+            border-slate-800 bg-[length:200%_100%] px-6
             font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 ${
               isHovered
-                ? "hover:bg-[linear-gradient(45deg,#040e44,45%,#120d57,55%,#160c5c)] scale-105 text-white uppercase"
+                ? "hover:bg-[linear-gradient(45deg,#040e44,45%,#120d57,55%,#160c5c)] scale-101 text-white uppercase"
                 : ""
-            }`}
+            } ${customCSS ? customCSS : ""}`}
       onMouseEnter={handleMouseMovement}
       onMouseLeave={handleMouseMovement}
     >
