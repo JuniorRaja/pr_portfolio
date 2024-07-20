c891193213f7b70194c2d54b9aa44b5567722495
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-07-21 03:14:57 +0530

    feat: Add lazy loading for images in gallery page

--- app/gallery/[route]/page.tsx
+++ app/gallery/[route]/page.tsx
@@ -1,7 +1,10 @@
+import ImageMouse from "@/components/ui/ImageMouse";
+
 const Page = ({ params }: { params: { route: string } }) => {
   return (
     <div className="min-h-screen w-full flex items-center justify-center">
       {params.route ? `Page name is: ${params.route}` : "Loading..."}
+      <ImageMouse />
     </div>
   );
 };
