e4c000ec5a1735f44cb4c04e927ebaedc0af6aed
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-07-22 08:09:00 +0530

    Feat : Simple gallery added with resources

--- app/gallery/[route]/page.tsx
+++ app/gallery/[route]/page.tsx
@@ -1,6 +1,83 @@
 import ImageMouse from "@/components/ui/ImageMouse";
+import { LayoutGrid } from "@/components/ui/layout-grid";
+import * as images from "../../../public/gallery/macro";
 
 const Page = ({ params }: { params: { route: string } }) => {
+  const InnerSkeleton = () => {
+    return (
+      <div>
+        <p className="font-bold md:text-4xl text-xl text-white">Image Title</p>
+        <p className="font-normal text-base text-white"></p>
+        <p className="font-normal text-base my-4 max-w-lg text-neutral-200">
+          Scene description
+        </p>
+      </div>
+    );
+  };
+
+  const cards = [
+    {
+      id: 1,
+      content: <InnerSkeleton />,
+      className: "md:col-span-1",
+      thumbnail: images.macro_1.src,
+    },
+    {
+      id: 2,
+      content: <InnerSkeleton />,
+      className: "col-span-1",
+      thumbnail: images.macro_2.src,
+    },
+    {
+      id: 3,
+      content: <InnerSkeleton />,
+      className: "col-span-1",
+      thumbnail: images.macro_3.src,
+    },
+    {
+      id: 4,
+      content: <InnerSkeleton />,
+      className: "md:col-span-1",
+      thumbnail: images.macro_4.src,
+    },
+    {
+      id: 5,
+      content: <InnerSkeleton />,
+      className: "md:col-span-1",
+      thumbnail: images.macro_5.src,
+    },
+    {
+      id: 6,
+      content: <InnerSkeleton />,
+      className: "col-span-1",
+      thumbnail: images.macro_6.src,
+    },
+    {
+      id: 7,
+      content: <InnerSkeleton />,
+      className: "col-span-1",
+      thumbnail: images.macro_7.src,
+    },
+    {
+      id: 8,
+      content: <InnerSkeleton />,
+      className: "md:col-span-1",
+      thumbnail: images.macro_8.src,
+    },
+    {
+      id: 9,
+      content: <InnerSkeleton />,
+      className: "col-span-1",
+      thumbnail: images.macro_9.src,
+    },
+    {
+      id: 10,
+      content: <InnerSkeleton />,
+      className: "md:col-span-1",
+      thumbnail: images.macro_10.src,
+    },
+  ];
+
   return (
     <div className="max-w-7xl w-full">
       <section className="w-full py-20 min-h-screen pt-36">
@@ -13,7 +90,8 @@ const Page = ({ params }: { params: { route: string } }) => {
             Gallery
           </h1>
         </div>
-        <ImageMouse />
+        {/* <ImageMouse /> */}
+        <LayoutGrid cards={cards} />
       </section>
     </div>
   );
---
+++ public/gallery/macro/index.tsx
@@ -0,0 +1,23 @@
+import macro_1 from "./macro_ (1).webp";
+import macro_2 from "./macro_ (2).webp";
+import macro_3 from "./macro_ (3).webp";
+import macro_4 from "./macro_ (4).webp";
+import macro_5 from "./macro_ (5).webp";
+import macro_6 from "./macro_ (6).webp";
+import macro_7 from "./macro_ (7).webp";
+import macro_8 from "./macro_ (8).webp";
+import macro_9 from "./macro_ (9).webp";
+import macro_10 from "./macro_ (10).webp";
+
+export {
+  macro_1,
+  macro_2,
+  macro_3,
+  macro_4,
+  macro_5,
+  macro_6,
+  macro_7,
+  macro_8,
+  macro_9,
+  macro_10,
+};
