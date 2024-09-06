33dae305a1ddb2a30717344f72d34c52682c38d8
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-09-06 08:43:24 +0530

    feat: Add lazy loading for images in gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,7 +1,37 @@
 "use client";
 import Carousel from "@/components/ui/Carousel";
+import { useState, useEffect } from "react";
+import { fetchAlbumsList } from "./../lib/fetchAlbumsList";
+
+interface Album {
+  id: number;
+  name: string;
+  likes: number;
+  views: number;
+  img: string;
+  route: string;
+  des: string;
+}
 
 const Gallery = () => {
+  const [albums, setAlbums] = useState<Album[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function getAlbums() {
+      try {
+        setLoading(true);
+        const data = await fetchAlbumsList();
+        setAlbums(data);
+      } catch (error) {
+        console.log("Failed to fetch albums");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getAlbums();
+  }, []);
   return (
     <div className="max-w-7xl w-full">
       <section className="w-full py-20 min-h-screen pt-36">
@@ -14,8 +44,8 @@ const Gallery = () => {
           that you can never master. I have tried to categorize my photos and my
           faviourtes of them are Minimal, Doors & Patterns.
         </p>
-        <div className="h-auto min-h-[45rem] md:max-w-[90%] m-auto">
-          <Carousel />
+        <div className="h-auto min-h-[45rem] md:max-w-[90%] m-auto text-center">
+          {loading ? <p>Loading...</p> : <Carousel albums={albums} />}
         </div>
         {/* Serach feature will be added soon
         <div className="flex justify-center mb-8">
@@ -24,34 +54,6 @@ const Gallery = () => {
             placeholder="Search"
             className="w-full max-w-md p-2 border border-border rounded bg-input text-foreground shadow-md"
           />
-        </div> */}
-        {/* 
-        New Slider implementated. hence commented the old
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {albums.map((album) => (
-            <Link key={album.route} href={`/gallery/${album.route}`} passHref>
-              <div
-                key={album.route}
-                className="bg-card rounded-md overflow-hidden shadow-lg transform hover:scale-105 transition-transform duration-300"
-              >
-                <img
-                  src={album.img.src}
-                  alt={album.name}
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h2 className="text-xl font-bold text-white">{album.name}</h2>
-                  <p className="text-muted-foreground">
-                    {album.likes} likes · {album.views} views
-                  </p>
-                </div>
-              </div>
-            </Link>
-          ))}
-        </div> */}
-
-        {/* <div className="flex items-center justify-center mt-40 relative">
-          <ImageSlider />
         </div> */}
       </section>
     </div>
