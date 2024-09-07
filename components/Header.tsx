bf26e056b17ffc4ecf38d9330526cf2811c8f2da
Prasanna Rajendran <imprasannarajendran@gmail.com>
2024-09-07 18:03:34 +0530

    Feat : State Mangement with Redux --complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PRLogoW from "../public/PRLogoW.png";
 import MenuSvg from "./ui/MenuSvg";
 import Image from "next/image";
 import Link from "next/link";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/store";
+import { setPrimaryColor } from "@/store/colorSlice";
+
+// const Skeleton = ({ classNames }: { classNames?: string }) => {
+//   return (
+//     <div className={`animate-pulse flex gap-4 w-full ${classNames}`}>
+//       <div className="bg-gray-700 h-4 w-20 rounded-xl"></div>
+//       <div className="bg-gray-700 h-4 w-20 rounded-xl"></div>
+//       <div className="bg-gray-700 h-4 w-20 rounded-xl"></div>
+//       <div className="bg-gray-700 h-4 w-20 rounded-xl"></div>
+//     </div>
+//   );
+// };
 
 interface NavbarItem {
   id: number;
@@ -17,16 +31,43 @@ interface NavbarProps {
 }
 
 const Header = ({ navItems }: NavbarProps) => {
+  const dispatch = useDispatch();
   const [activeNav, setActiveNav] = useState<number>(99);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const [openNav, setOpenNav] = useState<boolean>(false);
+  const primaryColor = useSelector(
+    (state: RootState) => state.color.primaryColor
+  );
+
+  useEffect(() => {
+    const savedColor = sessionStorage.getItem("primaryColor");
+    if (savedColor) {
+      dispatch(setPrimaryColor(savedColor));
+    } else {
+      dispatch(setPrimaryColor("#8B5CF6"));
+    }
+  }, [dispatch]);
 
   const toggleNav = () => {
     setOpenNav(!openNav);
   };
 
-  const handleClick = (id: number) => () => {
-    setActiveNav(id);
-    setOpenNav(false);
+  const handleClick =
+    (id: number) => (event: React.MouseEvent<HTMLAnchorElement>) => {
+      setActiveNav(id);
+      setOpenNav(false);
+    };
+
+  const colors = [
+    { code: "#8B5CF6", name: "Purple" },
+    { code: "#EC4899", name: "Pink" },
+    { code: "#F59E0B", name: "Amber" },
+  ];
+
+  const handleColorSelect = (color: string) => {
+    dispatch(setPrimaryColor(color));
+    sessionStorage.setItem("primaryColor", color);
+    setDropdownOpen(false);
   };
 
   return (
@@ -103,6 +144,31 @@ const Header = ({ navItems }: NavbarProps) => {
             </div>
           </div>
         </nav>
+        <div className="relative bg-white p-1 rounded-3xl hidden">
+          <div
+            className="w-8 h-8 rounded-full cursor-pointer bg-black-100"
+            style={{ backgroundColor: primaryColor }}
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+          />
+          {dropdownOpen && (
+            <div className="absolute mt-2 w-auto p-2 rounded-lg bg-white shadow-lg transform transition-all duration-300 ease-in-out">
+              {colors.map((color) => (
+                <div
+                  key={color.name}
+                  className="flex items-center px-4 py-1 justify-evenly hover:bg-gray-200 hover:rounded-lg gap-2"
+                >
+                  <div
+                    key={color.name}
+                    className="w-8 h-8 rounded-full my-2 cursor-pointer transition-transform transform hover:scale-110"
+                    style={{ backgroundColor: color.code }}
+                    onClick={() => handleColorSelect(color.code)}
+                  />
+                  <div className="text-black w-10">{color.name}</div>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
         <a
           href="#signup"
           className="button hidden mr-8 text-white/50 transition-colors hover:text-white lg:block"
--- store/index.ts
+++ store/index.ts
@@ -17,6 +17,15 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        // Ignore paths in state that we don't want to check
+        ignoredPaths: ["register"],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
