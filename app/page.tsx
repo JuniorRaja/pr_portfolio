a4bb0a364764532915886c1039bb13a93641f479
Prasanna Rajendran <imprasannarajendran@gmail.com>
2025-09-04 23:13:24 +0530

    Refactor layout and comment out unused code in Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,21 @@ export default function Home() {
   return (
     <div className="max-w-7xl w-full">
       <Hero />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <Grid />
     </div>
   );
